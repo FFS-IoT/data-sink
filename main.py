58d54861c949e3fd3f9e4a616c56bbd20a544e83
Yannik <me@adnidor.de>
2018-06-01 17:17:38 +0200

    added logging

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import paho.mqtt.client as mqtt
 import json
 import configparser
+import logging
+
 
 CONFIG_FILE = "input.conf"
 
@@ -10,14 +12,17 @@ config.read("input.conf")
 
 BASE_TOPIC = config['mqtt']["base_topic"]
 HOST = config['mqtt']['host']
+LOGFILE = config['logging']['file']
+
+logging.basicConfig(filename=LOGFILE,level=logging.DEBUG)
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    logging.info("Connected with result code "+str(rc))
     client.subscribe(BASE_TOPIC+'#')
     client.message_callback_add(BASE_TOPIC+'#',on_message)
 
 def on_message(client, userdata, msg):
-    print("Recieved",msg.topic,str(msg.payload))
+    logging.debug("Recieved "+msg.topic+" "+str(msg.payload))
 
 client = mqtt.Client()
 client.on_connect = on_connect
