7c7851616f09b9f381fba370faec3024d1978226
Yannik <me@adnidor.de>
2018-06-01 18:55:54 +0200

    handle timestamp

--- datapoint.py
+++ datapoint.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+import datetime
 
 class DataPoint():
-    def __init__(self, sensorid, channel, value):
+    def __init__(self, sensorid, channel, value, timestamp=None):
         self.sensorid = sensorid
         self.channel = channel
         self.value = value
+        self.timestamp = timestamp
+        if self.timestamp is None:
+            self.timestamp = datetime.datetime.now(datetime.timezone.utc)
 
     def __str__(self):
-        return str(self.sensorid)+"."+str(self.channel)+": "+str(self.value)
+        return str(self.sensorid)+"."+str(self.channel)+": "+str(self.value)+" @"+str(self.timestamp)
